fb2ee71fa9307d9ebea6d08915cde4e96b7887f1
Zin0218 <Zin_s@outlook.jp>
2026-04-18 23:05:29 +0900

    Create abc005-a.cpp
    https://atcoder.jp/contests/abc005/submissions/75092100

---
+++ archive/Saji0545/abc005-a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+int main()
+{
+	ull x,y;
+	cin >> x >> y;
+	cout << y / x;
+	putchar('\n');
+}
