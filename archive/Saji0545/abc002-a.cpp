2e85bdbc915f2dbda6543220c2e7d0ce1b1e3f2f
Zin0218 <Zin_s@outlook.jp>
2026-04-18 22:54:16 +0900

    Create abc002-a.cpp
    https://atcoder.jp/contests/abc002/submissions/75091334

---
+++ archive/Saji0545/abc002-a.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+int main()
+{
+	ull X, Y;
+	cin >> X >> Y;
+	printf("%d\n", (X > Y) ? X : Y);
+}
