514e81c6fe776397d4e2db02599fccfa97c8a84e
Zin0218 <Zin_s@outlook.jp>
2026-04-18 23:03:05 +0900

    Create abc004-a.cpp
    https://atcoder.jp/contests/abc004/submissions/75091943

---
+++ archive/Saji0545/abc004-a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+int main()
+{
+	ull N;
+	cin >> N;
+	cout << (2 * N);
+	putchar('\n');
+}
